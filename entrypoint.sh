14580998276507588999cdf6f06d470c75255d46
siddhantprateek <siddhantprateek@gmail.com>
2025-01-16 22:10:26 +0530

    updated README

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Run migrations
-bunx prisma migrate deploy
-
-# Start the application
-exec bun run src/index.ts
--- prisma/migrations/20250104070928_decoupled_investigation/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "investigations" DROP CONSTRAINT "investigations_protocolId_fkey";
-
--- AlterTable
-ALTER TABLE "investigations" ALTER COLUMN "protocolId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "investigations" ADD CONSTRAINT "investigations_protocolId_fkey" FOREIGN KEY ("protocolId") REFERENCES "response_protocols"("id") ON DELETE SET NULL ON UPDATE CASCADE;
