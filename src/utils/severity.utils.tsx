4bb314c424f39c6b9494bd8d4e34144488ce99e0
siddhantprateek <siddhantprateek@gmail.com>
2025-04-02 09:13:36 +0530

    updated severity color

--- src/utils/severity.utils.tsx
+++ src/utils/severity.utils.tsx
@@ -8,32 +8,32 @@ export const getSeverityColors = (severity: string): SeverityColors => {
   switch (severity.toUpperCase()) {
     case 'CRITICAL':
       return {
-        text: 'text-red-700',
-        border: 'border-red-500 dark:border-red-500/20',
+        text: 'text-red-600',
+        border: 'border-none dark:border-none',
         background: 'bg-red-100 dark:bg-red-600/20'
       };
     case 'HIGH':
       return {
         text: 'text-red-700',
-        border: 'border-red-300 dark:border-red-300/20',
-        background: 'bg-red-50 dark:bg-red-300/20'
+        border: 'border-none dark:border-none',
+        background: 'bg-red-50 dark:bg-red-500/10'
       };
     case 'MEDIUM':
       return {
-        text: 'text-yellow-800 dark:text-yellow-500/50',
-        border: 'border-yellow-300 dark:border-yellow-300/20',
+        text: 'text-yellow-600 dark:text-yellow-500',
+        border: 'border-none dark:border-none',
         background: 'bg-yellow-50 dark:bg-yellow-300/20'
       };
     case 'LOW':
       return {
-        text: 'text-blue-700',
-        border: 'border-blue-300 dark:border-blue-300/20',
-        background: 'bg-blue-50 dark:bg-blue-300/20'
+        text: 'text-blue-600',
+        border: 'border-none dark:border-none',
+        background: 'bg-blue-50 dark:bg-blue-600/20'
       };
     default:
       return {
         text: 'text-green-700',
-        border: 'border-green-300 dark:border-green-300/20',
+        border: 'border-none dark:border-none',
         background: 'bg-green-100 dark:bg-green-800/20'
       };
   }
