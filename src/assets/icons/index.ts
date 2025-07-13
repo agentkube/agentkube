c38c00ec6df7e592f1085347bfc0cac4d5fad9d3
siddhantprateek <siddhantprateek@gmail.com>
2025-07-13 12:14:18 +0530

    resources for provisioning

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -5,3 +5,10 @@ export { default as MetaAI } from './metaai';
 export { default as OpenAI } from './openai';
 export { default as OpenRouter } from './openrouter';
 export { default as Anthropic } from './anthropic';
+
+export { default as Github } from './github';
+export { default as Terraform } from './terraform';
+export { default as Ansible } from './ansible';
+export { default as Terragrunt } from './terragrunt.png';
+export { default as OpenTofu } from './opentofu';
+export { default as Pulumi } from './pulumi';
