facc9d316f8aebb4486b4e0cf8a23f5514ddc861
siddhantprateek <siddhantprateek@gmail.com>
2025-11-07 13:37:54 +0530

    updated tips

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,12 +4,12 @@ export { default as PROMETHEUS_OPERATOR } from './resources/prometheus-operator.
 
 
 // TIPS Assets
-export { default as WELCOME } from './tipsassets/welcome.png';
-export { default as TIPS1 } from './tipsassets/tips1.png';
-export { default as TIPS2 } from './tipsassets/tips2.png';
-export { default as TIPS3 } from './tipsassets/tips3.png';
-export { default as TIPS4 } from './tipsassets/tips4.png';
-export { default as TIPS5 } from './tipsassets/tips5.png';
-export { default as TIPS6 } from './tipsassets/tips6.png';
+export { default as WELCOME } from './tipsassets/welcome.jpg';
+export { default as TIPS1 } from './tipsassets/tips1.jpg';
+export { default as TIPS2 } from './tipsassets/tips2.jpg';
+export { default as TIPS3 } from './tipsassets/tips3.jpg';
+export { default as TIPS4 } from './tipsassets/tips4.jpg';
+export { default as TIPS5 } from './tipsassets/tips5.jpg';
+export { default as TIPS6 } from './tipsassets/tips6.jpg';
 
 export { default as AGENTKUBE } from './agentkube.svg';
